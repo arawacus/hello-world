e3469e408a002ebbfcbab7dcfd847aa14a7ddd6e
Asya Mosyagina <arawacus@gmail.com>
2021-07-27 13:41:06 -0700

    begining code

---
+++ letterFrequency.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def letterFreq(self, s):
+        
+        return s
+
+
+
+# s = input("Введите строку\n")
+s = "abracodabra boo"
+Solution = Solution()
+res = Solution.letterFreq(s)
+print("res = {}".format(res))
