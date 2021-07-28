ed62898fa1d5e1e0d8ec4cfe10d3cf6ec80afccf
Asya Mosyagina <arawacus@gmail.com>
2021-07-28 08:55:46 -0700

    Write result to file

--- letterFrequency.py
+++ letterFrequency.py
@@ -5,11 +5,11 @@ def letterFreq(s):
     freq = {}
     for lett in letters:
         freq[lett] = s.count(lett)
+    k = sum(freq.values())
+    print("total {}".format(k))
+    sortedFreq = sorted(freq.items(), key = lambda kv: kv[1])
 
-    print(freq)
-    sorted_freq = sorted(freq.items(), key = lambda kv: kv[1])
-
-    return sorted_freq
+    return sortedFreq
 
 # path = "/Users/asya/GitHub/hello-world/text.txt"
 
@@ -17,11 +17,15 @@ s = "abracodabra boo"
 f2 = open("text2.txt", 'w')
 f2.write(s)
 f2.close()
-f = open('text2.txt')
+f = open('text.txt')
 st = f.read()
 
 # s = input("Введите строку\n")
 # s = "abracodabra boo"
 
 res = letterFreq(st)
+resStr = str(res)
+f3 = open("text3.txt", 'a')
+f3.write(resStr)
+f3.close()
 print("res = {}".format(res))
